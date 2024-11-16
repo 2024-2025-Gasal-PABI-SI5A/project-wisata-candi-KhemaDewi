a895d11048c87f1fa3faf45ca196f73d0927edf7
Mustofa Akhyar <161103864+KhemaDewi@users.noreply.github.com>
2024-11-16 10:29:41 +0700

    update sign up

--- wisata_candi/lib/main.dart
+++ wisata_candi/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:wisata_candi/data/candi_data.dart';
 import 'package:wisata_candi/screens/detail_screen.dart';
 import 'package:wisata_candi/screens/profile_screen.dart';
 import 'package:wisata_candi/screens/signin_screen.dart';
+import 'package:wisata_candi/screens/signup_screen.dart';
 
 void main() {
   runApp(const MainApp());
@@ -31,11 +32,12 @@ class MainApp extends StatelessWidget {
         ),
         useMaterial3: true,
       ),
-      home: DetailScreen(
-        candi: candiList[0],
-      ),
+      // home: DetailScreen(
+      //   candi: candiList[0],
+      // ),
       // home: ProfileScreen(),
       // home: SignInScreen(),
+      home: SignUpScreen(),
     );
   }
 }
